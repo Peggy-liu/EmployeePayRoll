bddb8ad83dd41ad2bfdb371b845ab32e9ef02ead
Peggy-liu <101808315@student.swin.edu.au>
2020-08-17 16:35:18 +1000

    oauth2 keycloak implementation

--- src/main/java/com/payroll/example/EmployeePayrollApplication.java
+++ src/main/java/com/payroll/example/EmployeePayrollApplication.java
@@ -4,22 +4,12 @@ package com.payroll.example;
 
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootApplication
-public class EmployeePayrollApplication implements CommandLineRunner {
-	@Autowired
-	private ApplicationContext context;
+public class EmployeePayrollApplication  {
 	
-	 private static Logger LOG = (Logger) LoggerFactory
-		      .getLogger(EmployeePayrollApplication.class);
 
 	public static void main(String[] args) {
 		
@@ -30,11 +20,6 @@ public class EmployeePayrollApplication implements CommandLineRunner {
 		
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
 	
-		
-		LOG.warn(context.getBean(AuthenticationManager.class).toString());
-	}
 
 }
--- src/main/java/com/payroll/example/security/SecurityConfig.java
+++ src/main/java/com/payroll/example/security/SecurityConfig.java
@@ -1,78 +1,31 @@
 package com.payroll.example.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.payroll.example.service.MyUserDetailService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
 @Configuration
-@EnableWebSecurity
+@EnableResourceServer
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Value("${jwk-url}")
+	private String jwk_url;
+@Bean
+public TokenStore tokenStore() {
+	return new JwkTokenStore(jwk_url);
+}
 
-	@Autowired
-	private MyUserDetailService service;
-//	
-//	@Autowired
-//	JwtAuthenticationFilter authenticationFilter;
-	
+@Override
+public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+	resources.tokenStore(tokenStore()).resourceId("payroll");
+}
 
 
-	@Autowired
-	JwtAuthorizationFilter authorizationFilter;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// auth.authenticationProvider(getProvider());
-		auth.inMemoryAuthentication().withUser("user").password("user")
-				.authorities(new SimpleGrantedAuthority("ROLE_USER"));
-	}
-
-	
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
-	@Bean
-	public PasswordEncoder encoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
-
-	@Bean
-	public DaoAuthenticationProvider getProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(encoder());
-		provider.setUserDetailsService(service);
-
-		return provider;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.addFilterAt(new JwtAuthenticationFilter(authenticationManagerBean()),UsernamePasswordAuthenticationFilter.class)
-		        //.addFilterAt(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-				.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class).authorizeRequests()
-				.antMatchers("/login").permitAll().antMatchers("/orders/**").hasRole("USER")
-				.antMatchers("/employees/**").hasRole("ADMIN")
-				;
-	
-		// .anyRequest().authenticated();
-
-	}
 
 }
